fff8e23b80fa64ed207e721977bc22f78d23ce7d
Dominik Bauernfeind <bauernfeind.dominik@gmail.com>
2019-01-29 07:17:50 +0100

    allow for an alias on a sort query

--- src/Sort.php
+++ src/Sort.php
@@ -14,32 +14,41 @@ class Sort
     /** @var string */
     protected $property;
 
-    public function __construct(string $property, $sortClass)
+    /** @var string  */
+    protected $columnName;
+
+    public function __construct(string $property, $sortClass, ? string $columnName)
     {
         $this->property = $property;
         $this->sortClass = $sortClass;
+        $this->columnName = $columnName ?? $property;
     }
 
     public function sort(Builder $builder, $descending)
     {
         $sortClass = $this->resolveSortClass();
 
-        ($sortClass)($builder, $descending, $this->property);
+        ($sortClass)($builder, $descending, $this->columnName);
     }
 
-    public static function field(string $property) : self
+    public static function field(string $property, ? string $columnName = null) : self
     {
-        return new static($property, SortsField::class);
+        return new static($property, SortsField::class, $columnName);
     }
 
-    public static function custom(string $property, $sortClass) : self
+    public static function custom(string $property, $sortClass, ? string $columnName = null) : self
     {
-        return new static($property, $sortClass);
+        return new static($property, $sortClass, $columnName);
     }
 
     public function getProperty(): string
     {
         return $this->property;
+    }
+
+    public function getColumnName(): string
+    {
+        return $this->columnName;
     }
 
     public function isForProperty(string $property): bool
